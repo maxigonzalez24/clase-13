880baf19a68e423f25937c6e42b94cb871751f8e
Maxi Gonzalez <maxiadgonzalez@gmail.com>
2023-07-07 18:27:01 -0300

    etapa cumplida

--- etapa5.js
+++ etapa5.js
@@ -9,3 +9,35 @@ los autos que ya fueron vendidos.
 Para comenzar, tenés que agregar el código que escribiste en el ejercicio anterior. 
 Tené en cuenta que estamos optimizando nuestro código, por lo cual, 
 deberíamos utilizar el método filter.*/ 
+
+const autosImportados = require('./etapa1');
+
+ const concesionaria = {
+    autos: autosImportados,
+    
+    buscarAuto: function(patente) {
+      for (let i = 0; i < this.autos.length; i++) {
+        if (this.autos[i].patente === patente) {
+          return this.autos[i];
+        }
+      }
+      return null;
+    },
+  
+    venderAuto: function(patente) {
+      const autoEncontrado = this.buscarAuto(patente);
+      if (autoEncontrado) {
+        autoEncontrado.vendido = true;
+        console.log("El auto con patente " + patente + " ha sido vendido.");
+      } else {
+        console.log("No se encontró ningún auto con la patente " + patente + ".");
+      }
+    },
+    autosParaLaVenta: function() {
+        return this.autos.filter(auto => !auto.vendido);
+      }
+    };
+    
+    // Ejemplo de uso de la función autosParaLaVenta:
+    const autosDisponibles = concesionaria.autosParaLaVenta();
+    console.log(autosDisponibles);
