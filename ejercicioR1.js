c02c9022708bad3f93e0f9c7308539f679c1e81f
Rafael <rafael.castro@mi.unc.edu.ar>
2023-07-07 12:54:05 -0300

    ultima modificacion

--- ejercicioR1.js
+++ ejercicioR1.js
@@ -1,12 +1,9 @@
+//EJEMPLO CALCUDORA
 // SUMA
 let a = 20;
 let b = 30;
 let suma = a + b;
 console.log(suma);
-<<<<<<< HEAD
-
-
-
 // MULTIPLICACION
 let x = 15;
 let y = 5;
@@ -21,4 +18,3 @@ let c = 100;
 let d = 50;
 let resta = c + d;
 console.log(resta);
->>>>>>> c1a3e134cf1c5e741de8a35eb9f4ae34eded0168
