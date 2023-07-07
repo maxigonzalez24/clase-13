6164518d8e802473a11f878e9192b726aab9156b
Rafael <rafael.castro@mi.unc.edu.ar>
2023-07-07 12:42:34 -0300

    modificacion ejercicio 7

--- ejercicioR1.js
+++ ejercicioR1.js
@@ -2,3 +2,14 @@ let a = 20;
 let b = 30;
 let suma = a + b;
 console.log(suma);
+
+
+
+// MULTIPLICACION
+let x = 15;
+let y = 5;
+let producto = x * y;
+console.log(producto);
+// DIVISION
+let division = x / y;
+console.log(division);
